11f3b6e9d41c968d6888e01c18d613d0601953d9
ale94 <alejandrorua111@gmail.com>
2024-09-30 21:41:44 -0300

    feat: user-form component created

--- src/app/components/user-app/user-app.component.ts
+++ src/app/components/user-app/user-app.component.ts
@@ -2,20 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { UserComponent } from '../user/user.component';
+import { UserFormComponent } from '../user-form/user-form.component';
 
 @Component({
   selector: 'user-app',
   standalone: true,
-  imports: [UserComponent],
+  imports: [UserComponent, UserFormComponent],
   templateUrl: './user-app.component.html',
 })
 export class UserAppComponent implements OnInit {
-  title = 'Listado de usuarios!';
+  title = 'User List!';
   users: User[] = [];
 
   constructor(private userService: UserService) {}
 
   ngOnInit(): void {
     this.userService.findAll().subscribe((users) => (this.users = users));
+  }
+
+  addUser(user: User) {
+    this.users = [...this.users, { ...user }];
   }
 }
